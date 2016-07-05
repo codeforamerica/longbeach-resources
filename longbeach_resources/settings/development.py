3425f27ac1b84fca12b5ce66893325738bcfa2e3
Mark Rossetti <mark.a.rossetti@gmail.com>
2016-07-05 13:21:44 -0700

    Corrected directory structure by flattening it one level

---
+++ longbeach_resources/settings/development.py
@@ -0,0 +1,3 @@
+from longbeach_resources.settings.development import *
+
+COMPRESS_ENABLE = True
--- longbeach_resources/settings/vagrant.py
+++ longbeach_resources/settings/vagrant.py
@@ -1,5 +1,3 @@
 from longbeach_resources.settings.environment import *
 
 COMPRESS_ENABLE = True
-
-LANGUAGE_CODE = 'es-pr'
